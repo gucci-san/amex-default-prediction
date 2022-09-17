14b08cdcc4af24b942e6c4f8bb8b176f465e798e
guccisan <yamskymtb0903@gmail.com>
2022-09-17 21:15:00 +0900

    raddar_03のコピー

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 # Linux --
 DATA = "/mnt/sdb/KAGGLE_DATA/amex-default-prediction"
 PICKLE = "_pickle"
+PARQUET = "_parquet"
 
 train_data_pickle = os.path.join(DATA, PICKLE, "train_data.pkl")
 test_data_pickle = os.path.join(DATA, PICKLE, "test_data.pkl")
+train_data_parquet = os.path.join(DATA, PARQUET, "train.parquet")
+test_data_parquet = os.path.join(DATA, PARQUET, "test.parquet")
