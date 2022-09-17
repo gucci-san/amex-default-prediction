002a218002d3cb7d699f4b73908ef78f4bc1391e
guccisan <yamskymtb0903@gmail.com>
2022-09-17 15:57:41 +0900

    raddarのparquet作成

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
 # Linux --
 DATA = "/mnt/sdb/KAGGLE_DATA/amex-default-prediction"
 PICKLE = "_pickle"
+
+train_data_pickle = os.path.join(DATA, PICKLE, "train_data.pkl")
+test_data_pickle = os.path.join(DATA, PICKLE, "test_data.pkl")
