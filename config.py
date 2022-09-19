ae2a3d7a12ea551f62f3834aa2ee5bf78312dbe2
guccisan <yamskymtb0903@gmail.com>
2022-09-19 16:25:46 +0900

    S1がkillされる問題の検証まで

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ DATA = "/mnt/sdb/KAGGLE_DATA/amex-default-prediction"
 PICKLE = "_pickle"
 PARQUET = "_parquet"
 
+train_data_csv = os.path.join(DATA, "train_data.csv")
+test_data_csv = os.path.join(DATA, "test_data.csv")
 train_data_pickle = os.path.join(DATA, PICKLE, "train_data.pkl")
 test_data_pickle = os.path.join(DATA, PICKLE, "test_data.pkl")
 train_data_parquet = os.path.join(DATA, PARQUET, "train.parquet")
